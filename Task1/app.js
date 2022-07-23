33746c4aeafac78422fbd87e5d1bfd6f973abe71
Anish Patil <anishpatil304@gmail.com>
2022-07-23 20:22:25 +0530

    first-commit

---
+++ Task1/app.js
@@ -0,0 +1,24 @@
+function number(num)
+{
+    var result = document.getElementById("input");
+    result.value += num;
+    
+
+}
+function result(){
+    var result = document.getElementById("input");
+    result.value=eval(result.value)
+}
+
+function clearresult()
+{
+    var result = document.getElementById("input");
+    result.value="";
+}
+function deletechar()
+{
+    var number = document.getElementById("input");
+    var remove = number.value;
+    remove = remove.slice(0,-1);
+    number.value=remove;
+}
